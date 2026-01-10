d856101d57e0ed3792ca7284f5cae0883b98c7fc
Karl <git@karlneugebauer.com>
2026-01-10 01:48:54 +0100

    Moved mod settings setup to settings class

--- MyHeadTrackingCar/MyHeadTrackingCarMod.cs
+++ MyHeadTrackingCar/MyHeadTrackingCarMod.cs
@@ -21,7 +21,7 @@ public class MyHeadTrackingCarMod : Mod
 
     #endregion
 
-    private readonly MyHeadTrackingCarSettings _settings = new();
+    private readonly MyHeadTrackingCarSettings _modSettings = new();
 
     #region ModLoader Hooks
 
@@ -32,81 +32,10 @@ public class MyHeadTrackingCarMod : Mod
         HarmonyInstance harmonyInstance = HarmonyInstance.Create("com.cineafx.myheadtrackingcar.patch");
         harmonyInstance.PatchAll();
 
-        SetupFunction(Setup.ModSettings, ModSettings);
+        SetupFunction(Setup.ModSettings, _modSettings.SetupModSettings);
         SetupFunction(Setup.OnLoad, OnLoad);
         SetupFunction(Setup.OnModEnabled, OnModEnabled);
         SetupFunction(Setup.OnModDisabled, OnModDisabled);
-    }
-
-    private void ModSettings()
-    {
-        Settings.AddHeader("General");
-        _settings.TrackingOutsideOfVehicles = Settings.AddCheckBox(
-            "headTrackingOutsideOfVehicles",
-            "Head tracking outside of vehicles",
-            true
-        );
-        _settings.ForceMouseOnHorizonLevelInVehicle = Settings.AddCheckBox(
-            "forceMouseOnHorizonLevelInVehicle",
-            "Force mouse to horizon level in vehicles",
-            false
-        );
-        _settings.ForceMouseOnHorizonLevelOnFoot = Settings.AddCheckBox(
-            "forceMouseOnHorizonLevelOnFoot",
-            "Force mouse to horizon level on foot",
-            false
-        );
-
-        Settings.CreateGroup();
-        Settings.AddHeader("Tracking configuration");
-        _settings.EnablePitch = Settings.AddCheckBox(
-            "enablePitch",
-            "Enable pitch",
-            true
-        );
-        _settings.EnableYaw = Settings.AddCheckBox(
-            "enableYaw",
-            "Enable yaw",
-            true
-        );
-        _settings.EnableRoll = Settings.AddCheckBox(
-            "enableRoll",
-            "Enable roll",
-            true
-        );
-        _settings.EnableX = Settings.AddCheckBox(
-            "enableX",
-            "Enable x (left <--> right)",
-            true
-        );
-        _settings.EnableY = Settings.AddCheckBox(
-            "enableY",
-            "Enable y (up <--> down)",
-            true
-        );
-        _settings.EnableZ = Settings.AddCheckBox(
-            "enableZ",
-            "Enable z (front <--> back)",
-            true
-        );
-
-        Settings.CreateGroup();
-        Settings.AddHeader("Lost tracking");
-        _settings.ShouldRecenterIfLostTracking = Settings.AddCheckBox(
-            "shouldRecenterIfLostTracking",
-            "Should recenter camera if tracking is lost",
-            true
-        );
-        _settings.LostTrackingTimeoutSeconds = Settings.AddSlider(
-            "lostTrackingTimeoutS",
-            "Lost tracking re-centering delay (seconds)",
-            0.2f, 5.0f, 1.0f,
-            null, 2);
-        _settings.LostTrackingRecenterDurationSeconds = Settings.AddSlider(
-            "lostTrackingRecenterDurationS",
-            "Re-centering duration (seconds)",
-            0.0f, 5.0f, 0.5f,
-            null, 2);
     }
 
     private void OnLoad()
@@ -138,7 +67,7 @@ public class MyHeadTrackingCarMod : Mod
     {
         GameObject playerGameObj = GameObject.Find("/PLAYER");
         _trackIrComponent = playerGameObj.AddComponent<MyHeadTrackingCarComponent>();
-        _trackIrComponent.Settings = _settings;
+        _trackIrComponent.Settings = _modSettings;
     }
 
     #endregion
--- MyHeadTrackingCar/MyHeadTrackingCarSettings.cs
+++ MyHeadTrackingCar/MyHeadTrackingCarSettings.cs
@@ -8,7 +8,7 @@ public class MyHeadTrackingCarSettings
     public SettingsCheckBox TrackingOutsideOfVehicles { get; set; }
     public SettingsCheckBox ForceMouseOnHorizonLevelInVehicle { get; set; }
     public SettingsCheckBox ForceMouseOnHorizonLevelOnFoot { get; set; }
-    
+
     // Tracking configuration
     public SettingsCheckBox EnablePitch { get; set; }
     public SettingsCheckBox EnableYaw { get; set; }
@@ -16,9 +16,80 @@ public class MyHeadTrackingCarSettings
     public SettingsCheckBox EnableX { get; set; }
     public SettingsCheckBox EnableY { get; set; }
     public SettingsCheckBox EnableZ { get; set; }
-    
+
     // Lost tracking
     public SettingsCheckBox ShouldRecenterIfLostTracking { get; set; }
     public SettingsSlider LostTrackingTimeoutSeconds { get; set; }
     public SettingsSlider LostTrackingRecenterDurationSeconds { get; set; }
+
+    internal void SetupModSettings()
+    {
+        Settings.AddHeader("General");
+        TrackingOutsideOfVehicles = Settings.AddCheckBox(
+            "headTrackingOutsideOfVehicles",
+            "Head tracking outside of vehicles",
+            true
+        );
+        ForceMouseOnHorizonLevelInVehicle = Settings.AddCheckBox(
+            "forceMouseOnHorizonLevelInVehicle",
+            "Force mouse to horizon level in vehicles",
+            false
+        );
+        ForceMouseOnHorizonLevelOnFoot = Settings.AddCheckBox(
+            "forceMouseOnHorizonLevelOnFoot",
+            "Force mouse to horizon level on foot",
+            false
+        );
+
+        Settings.CreateGroup();
+        Settings.AddHeader("Tracking configuration");
+        EnablePitch = Settings.AddCheckBox(
+            "enablePitch",
+            "Enable pitch",
+            true
+        );
+        EnableYaw = Settings.AddCheckBox(
+            "enableYaw",
+            "Enable yaw",
+            true
+        );
+        EnableRoll = Settings.AddCheckBox(
+            "enableRoll",
+            "Enable roll",
+            true
+        );
+        EnableX = Settings.AddCheckBox(
+            "enableX",
+            "Enable x (left <--> right)",
+            true
+        );
+        EnableY = Settings.AddCheckBox(
+            "enableY",
+            "Enable y (up <--> down)",
+            true
+        );
+        EnableZ = Settings.AddCheckBox(
+            "enableZ",
+            "Enable z (front <--> back)",
+            true
+        );
+
+        Settings.CreateGroup();
+        Settings.AddHeader("Lost tracking");
+        ShouldRecenterIfLostTracking = Settings.AddCheckBox(
+            "shouldRecenterIfLostTracking",
+            "Should recenter camera if tracking is lost",
+            true
+        );
+        LostTrackingTimeoutSeconds = Settings.AddSlider(
+            "lostTrackingTimeoutS",
+            "Lost tracking re-centering delay (seconds)",
+            0.2f, 5.0f, 1.0f,
+            null, 2);
+        LostTrackingRecenterDurationSeconds = Settings.AddSlider(
+            "lostTrackingRecenterDurationS",
+            "Re-centering duration (seconds)",
+            0.0f, 5.0f, 0.5f,
+            null, 2);
+    }
 }
